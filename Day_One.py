184dfea45cb22a2aff43b7965609bda5d7632cac
TLohan13 <89541491+TLohan13@users.noreply.github.com>
2021-12-31 11:16:16 -0500

    100 Days of Python - Day 1

---
+++ Day_One.py
@@ -0,0 +1,8 @@
+# 1) Creat Greeting
+print("Welcome to the band name generator!")
+# 2) Ask for city that user grew up in - made variable name to store data
+city = input("Which city did you grow up in?\n")
+# 3) Ask for pet name of pet they've had
+pet = input("What is the name of your pet?\n")
+# 4) Add City Name and Pet Name
+print("Your band name is " + city + " " + pet + "!")
