d27f72cc0212f291be7854cbccaf2180472e0f8f
TLohan13 <89541491+TLohan13@users.noreply.github.com>
2022-01-03 19:09:49 -0500

    Added Day 4 Project

--- Day4.py
+++ Day4.py
@@ -1,16 +1,62 @@
 import random
-import my_module
 
-# Day 4
+# Rock Paper Scissors
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+
+'''
+game_images = [rock, paper, scissors]
 
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+# What the user enters.
+user_choice = int(input(
+    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose")
+else:
 
-# print(my_module.pi)
+    print(game_images[user_choice])
 
-random_float = random.random()
-print(random_float)
+    # Computer's choice. Need random number generated, so will need to import random.
+    computer_choice = random.randint(0, 2)
+    print("Computer chose: ")
+    print(game_images[computer_choice])
 
-love_score = random.randint(1, 100)
-print(f"Your love score is {love_score}")
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose.")
+    elif computer_choice > user_choice:
+        print("You Lose.")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw!")
+    elif user_choice >= 3 or user_choice < 0:
+        print("You typed an invalid number, you lose")
